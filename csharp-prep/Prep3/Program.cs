08ca1265fdaf109733d99e0194192659cd9d18f8
David Keller <kelldav004@gmail.com>
2023-01-10 17:01:43 -0700

    Made some changes and did one stretch challenge.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        int totalGuesses = 0;
+
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,101);
+
+        while (play == "yes")
+        {
+            Console.Write("What is your guess? ");
+            string read2 = Console.ReadLine();
+            int guess = int.Parse(read2);
+            totalGuesses = totalGuesses + 1;
+
+            if (guess == randomNumber)
+            {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"The total guesses made was {totalGuesses}.");
+            Console.WriteLine("Thank you.");
+            play = "False";
+            }
+
+            else if (guess > randomNumber)
+            {
+            Console.WriteLine("Lower");
+            }
+
+            else
+            {
+            Console.WriteLine("Higher");
+            }
+        }
     }
 }
