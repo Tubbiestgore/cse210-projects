a0e4bb75c17024a40006a4461c7edc1bdd44c74a
David Keller <kelldav004@gmail.com>
2023-02-01 10:39:16 -0700

    Here is the final program.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction a = new Fraction();
+        Console.WriteLine(a.GetFractionString());
+        Console.WriteLine(a.GetDecimalValue());
+
+        Fraction b = new Fraction(5);
+        Console.WriteLine(b.GetFractionString());
+        Console.WriteLine(b.GetDecimalValue());
+
+        Fraction c = new Fraction(3,4);
+        Console.WriteLine(c.GetFractionString());
+        Console.WriteLine(c.GetDecimalValue());
+
+        Fraction d = new Fraction(1,3);
+        Console.WriteLine(d.GetFractionString());
+        Console.WriteLine(d.GetDecimalValue());
+
     }
 }
